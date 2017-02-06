5a0465abf545e982b31ec34627919f9d9d6660ca
sumit <op.sumitsheoran@gmail.com>
2017-02-06 10:19:58 +0530

    working on ui-grid

--- app/app.js
+++ app/app.js
@@ -7,7 +7,8 @@ angular.module('myApp', [
   'myApp.view2',
   // 'myApp.version',
   'myApp.members',
-  'myApp.tasks'
+  'myApp.tasks',
+  'ui.grid'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
--- app/view2/view2.js
+++ app/view2/view2.js
@@ -8,28 +8,32 @@ angular.module('myApp.view2', ['ngRoute'])
     controller: 'View2Ctrl'
   });
 }])
-.controller('View2Ctrl', function($scope, $window, Members) {
-	$scope.members = Members.getMembers();
-	$scope.member = "";
-	$scope.addMember = function() {
-      if ($scope.member !="") {
-        Members.setMember($scope.member);
-        $scope.member = "";
-      }
-      else {
-      	$window.alert("Enter a members name");
-      }
-    }
-    $scope.editMember = function (x) {
-        $scope.member = $scope.members[x];
-        // $scope.members.splice(x, 1);
-        Members.editMember(x);
-    }
-    $scope.removeMember = function (x) {
-        Members.deleteMember(x);
-        // $scope.members.splice(x, 1);
-    }
-});
+
+
+// .controller('View2Ctrl', function($scope, $window, Members) {
+// 	$scope.members = Members.getMembers();
+// 	$scope.member = "";
+// 	$scope.addMember = function() {
+//       if ($scope.member !="") {
+//         Members.setMember($scope.member);
+//         $scope.member = "";
+//       }
+//       else {
+//       	$window.alert("Enter a members name");
+//       }
+//     }
+//     $scope.editMember = function (x) {
+//         $scope.member = $scope.members[x];
+//         // $scope.members.splice(x, 1);
+//         Members.editMember(x);
+//     }
+//     $scope.removeMember = function (x) {
+//         Members.deleteMember(x);
+//         // $scope.members.splice(x, 1);
+//     }
+// });
+
+
 // Doubt : myApp.members was provided to myApp only, how is it accessible from myApp.view2 without giving it as provider and if it is why are we providing ngRoute?
 // .controller('View2Ctrl', function($scope, $window) {
 // 	$scope.members=[];
@@ -45,3 +49,15 @@ angular.module('myApp.view2', ['ngRoute'])
 //       }
 //     }
 // });
+
+
+.controller('View2Ctrl', function($scope, $window, Members) {
+	
+	$scope.addMember = function() {
+        Members.setMember($scope.member);
+        $scope.member = "";
+    }
+
+	$scope.myData = Members.getMembers();
+
+});
